1c622994b16aa9d85ba75933be031484addf3450
luccabracco <luccabraccoferreira@gmail.com>
2026-04-27 10:19:33 -0300

    Criando Interface no CMD

--- src/main.c
+++ src/main.c
@@ -2,7 +2,22 @@
 
 int main() {
     
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-    
-    return 0;
-}
+int main() {
+    int opcao;
+    float n1, n2, media;
+    float coef, exp;
+
+    do {
+        // --- (2) Interface no CMD ---
+        printf("\n=================================");
+        printf("\n   SISTEMA ACADEMICO - VERSAO 1.0");
+        printf("\n=================================");
+        printf("\n1. Calcular Media e Classificacao");
+        printf("\n2. Calcular Derivada (ax^n)");
+        printf("\n0. Sair");
+        printf("\n Escolha uma opcao: ");
+        scanf("%d", &opcao);
