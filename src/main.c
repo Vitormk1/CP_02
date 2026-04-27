2782c47aac0e9806905fc695cb8c2badc50308f7
jferrari-code <joaopffaian@gmail.com>
2026-04-27 11:27:05 -0300

    Implementação 03 da entrada valida

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int main() {
     srand(time(NULL));
 
     do {
-        entradavalida = 0; 
+        entradavalida = 0;
 
         printf("\n=================================");
         printf("\n   SISTEMA ACADEMICO - VERSAO 1.0");
@@ -23,30 +23,28 @@ int main() {
         printf("\n0. Sair");
         printf("\n Escolha uma opcao: ");
 
-    
         if (scanf("%d", &opcao) != 1) {
             printf("\n[ERRO]: Digite apenas NUMEROS!\n");
-
-    }
+            while (getchar() != '\n'); 
+            system("pause");
+            continue;
+        }
 
         switch(opcao) {
             case 1:
                 printf("\n--- CALCULO DE MEDIA ---");
                 printf("\nDigite a Nota 1: ");
-                scanf("%f", &n1);
+                if (scanf("%f", &n1) != 1) { printf("Entrada invalida!"); while(getchar() != '\n'); break; }
+                
                 printf("Digite a Nota 2: ");
-                scanf("%f", &n2);
+                if (scanf("%f", &n2) != 1) { printf("Entrada invalida!"); while(getchar() != '\n'); break; }
 
                 media = (n1 + n2) / 2;
                 printf("\nMedia Final: %.2f", media);
 
-                if (media >= 7.0) {
-                    printf("\nSituacao: APROVADO\n");
-                } else if (media >= 5.0) {
-                    printf("\nSituacao: RECUPERACAO\n");
-                } else {
-                    printf("\nSituacao: REPROVADO\n");
-                }
+                if (media >= 7.0) printf("\nSituacao: APROVADO\n");
+                else if (media >= 5.0) printf("\nSituacao: RECUPERACAO\n");
+                else printf("\nSituacao: REPROVADO\n");
 
                 system("pause");
                 break;
@@ -74,16 +72,14 @@ int main() {
 
             case 3:
                 printf("\n--- NUMERO ALEATORIO ---");
-
-                int numero = rand() % 100 + 1;
-                printf("Numero gerado: %d\n", numero);
-
+                int numAleatorio = rand() % 100 + 1;
+                printf("Numero gerado: %d\n", numAleatorio);
                 system("pause");
                 break;
 
             case 0:
                 printf("\nSaindo do sistema...\n");
-                exit(0);
+                break;
 
             default:
                 printf("\nOpcao invalida!\n");
